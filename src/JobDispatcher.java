ccc2ab15fe2fe6f2a79321c82fe779445c3b51af
Derek S S Wong <dsw6@hw.ac.uk>
2021-03-09 01:45:10 +0000

    Added JUnit tests & fixed bugs

--- src/JobDispatcher.java
+++ src/JobDispatcher.java
@@ -2,9 +2,15 @@
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 // Note that you MUST not use any other `thread safe` classes than the two imported above
-import java.util.Arrays;
 import java.util.ArrayList;
 
+/**
+ * My solution is to store individual jobs as objects in an arraylist,
+ *  and whenever a new job is specified, or a worker thread is queued,
+ *  the code will check if any current jobs are ready to be completed
+ * @author dsw6
+ *
+ */
 public class JobDispatcher implements Dispatcher {
 	
 	//Declare your local variables and objects here
@@ -13,28 +19,40 @@ public class JobDispatcher implements Dispatcher {
 	private Condition computeWait = lock.newCondition();
 	private Condition storageWait = lock.newCondition();
 	
-	private int ID = 0;
+	private int ID = 0; //Unique key for jobs
 	private ArrayList<Job> jobList = new ArrayList<Job>();
+	
+	private int waitingCompute = 0;
+	private int waitingStorage = 0;
 
 
 	@Override
 	public void specifyJob(int nComputeThreads, int nStorageThreads) {
 		//Your code here
 		lock.lock();
+		
+			//Creates a new instance of the Job class
 			int currentComputeThreads = lock.getWaitQueueLength(computeWait);
 			int currentStorageThreads = lock.getWaitQueueLength(storageWait);
 			boolean enoughCompute = (currentComputeThreads >= nComputeThreads);
 			boolean enoughStorage = (currentStorageThreads >= nStorageThreads);
 			Job job = new Job(ID, nComputeThreads, nStorageThreads, enoughCompute, enoughStorage);
+			System.out.println("(SJ) Job " + ID + "(" + job.getComputeThreads() + "," + job.getStorageThreads() + ")" +" added");
 			
-			if (enoughCompute == enoughStorage == true) {
+			//Checks if the new job is doable with the current amount of worker threads
+			//If so, signal the threads needed and finish the job
+			//If not, add the Job instance to the jobList
+			if (enoughCompute & enoughStorage) {
 				signalCompute(nComputeThreads);
 				signalStorage(nStorageThreads);
-				System.out.println("Job " + ID + " completed");
+				System.out.println("(SJ) Job " + ID + "(" + job.getComputeThreads() + "," + job.getStorageThreads() + ")" +" completed");
 			} 
-			else {jobList.add(job);}
+			else {
+				jobList.add(job);
+				System.out.println("(SJ) Job " + ID + "(" + job.getComputeThreads() + "," + job.getStorageThreads() + ")" +" queued");
+			}
 			
-			ID += 1;
+			ID += 1; //Increment ID for the next job
 		lock.unlock();
 	}
 
@@ -42,26 +60,33 @@ public class JobDispatcher implements Dispatcher {
 	public void queueComputeThread() {
 		//Your code here
 		lock.lock();
+		
 			int currentCompute = lock.getWaitQueueLength(computeWait) + 1;
 			int currentStorage = lock.getWaitQueueLength(storageWait);
+			System.out.println("Compute +1 = " + currentCompute);
 			ArrayList<Job> readyToRun = inspectList(currentCompute, currentStorage);
+			
+			//If there are doable jobs, remove them from the jobList and unlocks
+			//If not, go into waiting
 			if (readyToRun.size() > 0) {
 				for (Job i: readyToRun) {
 					signalCompute(i.getComputeThreads() - 1);
 					signalStorage(i.getStorageThreads());
-					System.out.println("Job " + i.getID() + " completed");
+					System.out.println("(CT) Job " + i.getID() + "(" + i.getComputeThreads() + "," + i.getStorageThreads() + ")" + " completed");
 					jobList.remove(i);
 				}
 			}
 			else {
 				try {
+					waitingCompute += 1;
 					computeWait.await();
+					waitingCompute -= 1;
+					
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
 		lock.unlock();
 		
 	}
@@ -72,18 +97,21 @@ public class JobDispatcher implements Dispatcher {
 		lock.lock();
 		int currentCompute = lock.getWaitQueueLength(computeWait);
 		int currentStorage = lock.getWaitQueueLength(storageWait) + 1;
+		System.out.println("Storage +1 = " + currentStorage);
 		ArrayList<Job> readyToRun = inspectList(currentCompute, currentStorage);
 		if (readyToRun.size() > 0) {
 			for (Job i: readyToRun) {
 				signalCompute(i.getComputeThreads());
 				signalStorage(i.getStorageThreads() - 1);
-				System.out.println("Job " + i.getID() + " completed");
+				System.out.println("(ST) Job " + i.getID() + "(" + i.getComputeThreads() + "," + i.getStorageThreads() + ")" + " completed");
 				jobList.remove(i);
 			}
 		}
 		else {
 			try {
+				waitingStorage += 1;
 				storageWait.await();
+				waitingStorage -= 1;
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -93,28 +121,52 @@ public class JobDispatcher implements Dispatcher {
 	lock.unlock();
 	}
 	
+	//Signals a specified amount of compute threads
 	private void signalCompute(int signal) {
+		lock.lock();
 		for (int i = 0; i < signal; i++) {
 			computeWait.signal();
 		}
+		lock.unlock();
 	}
 	
+	//Signals a specified amount of storage threads
 	private void signalStorage(int signal) {
+		lock.lock();
 		for (int i = 0; i < signal; i++) {
 			storageWait.signal();
 		}
+		lock.unlock();
 	}
 	
+	//Iterates through the jobList and returns a combination of jobs which can be completed with the current amount of threads
 	private ArrayList<Job> inspectList(int currentCompute, int currentStorage) {
-		ArrayList<Job> result = new ArrayList<Job>();
-			for (Job i : jobList) {
-				if (currentCompute >= i.getComputeThreads() & currentStorage >= i.getStorageThreads()) {
-					result.add(i);
-					currentCompute -= i.getComputeThreads();
-					currentStorage -= i.getStorageThreads();
+		lock.lock();
+			ArrayList<Job> result = new ArrayList<Job>();
+				for (Job i : jobList) {
+					if ((currentCompute >= i.getComputeThreads()) & (currentStorage >= i.getStorageThreads())) {
+						result.add(i);
+						currentCompute -= i.getComputeThreads();
+						currentStorage -= i.getStorageThreads();
+					}
 				}
-			}
+		lock.unlock();
 		return result;
+	}
+	
+	//Returns the number of currently waiting compute threads
+	public int getWaitingCompute() {
+		return waitingCompute;
+	}
+	
+	//Returns the number of currently waiting storage threads
+	public int getWaitingStorage() {
+		return waitingStorage;
+	}
+	
+	//Returns the size of the jobList
+	public int getJobListSize() {
+		return jobList.size();
 	}
 
 	//Add any private classes that you need
--- src/Tests.java
+++ src/Tests.java
@@ -44,9 +44,46 @@ class Tests {
 	 * 
 	 */
 	@org.junit.jupiter.api.Test
-	void specifyJobTest() {
+	void UR1Test() {
 		JobDispatcher dispatcher = new JobDispatcher();
 		
+		//Create compute threads
+		for (int i = 0; i < 4; i++) {
+			Thread computeThread = new Thread() {			
+				public void run () {
+					dispatcher.queueComputeThread();
+				}
+			};	
+			computeThread.start();
+		}
+		
+		//Create storage threads
+		for (int i = 0; i < 0; i++) {
+			Thread storageThread = new Thread() {			
+				public void run () {
+					dispatcher.queueStorageThread();
+				}
+			};	
+			storageThread.start();
+		}
+		
+		//Create job
+		dispatcher.specifyJob(4, 0);
+		
+		//Wait for set time and assume that execution has finished:
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace(); };
+		
+		assertEquals(0, dispatcher.getWaitingCompute());
+		assertEquals(0, dispatcher.getWaitingStorage());
+		assertEquals(0, dispatcher.getJobListSize());
+		
+	}
+	
+	@org.junit.jupiter.api.Test
+	void UR2Test() {
+		JobDispatcher dispatcher = new JobDispatcher();
+		
+		//Create compute threads
 		for (int i = 0; i < 5; i++) {
 			Thread computeThread = new Thread() {			
 				public void run () {
@@ -56,6 +93,7 @@ class Tests {
 			computeThread.start();
 		}
 		
+		//Create storage threads
 		for (int i = 0; i < 5; i++) {
 			Thread storageThread = new Thread() {			
 				public void run () {
@@ -65,13 +103,78 @@ class Tests {
 			storageThread.start();
 		}
 		
-		dispatcher.specifyJob(1, 0);
-		dispatcher.specifyJob(2, 2);
-		dispatcher.specifyJob(2, 0);
+		//Specify jobs
+		dispatcher.specifyJob(3, 1);
+		dispatcher.specifyJob(2, 1);
 		dispatcher.specifyJob(0, 3);
+		dispatcher.specifyJob(6, 6); //This one is supposed to be left hanging
 		
+		//Wait for set time and assume that execution has finished:
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace(); };
+		
+		assertEquals(0, dispatcher.getWaitingCompute());
+		assertEquals(0, dispatcher.getWaitingStorage());
+		assertEquals(1, dispatcher.getJobListSize());
 	}
 	
-	
+	@org.junit.jupiter.api.Test
+	void UR3Test() {
+		JobDispatcher dispatcher = new JobDispatcher();
+		
+		//Create compute threads
+		for (int i = 0; i < 2; i++) {
+			Thread computeThread = new Thread() {			
+				public void run () {
+					dispatcher.queueComputeThread();
+				}
+			};	
+			computeThread.start();
+		}
+		
+		//Create storage threads
+		for (int i = 0; i < 1; i++) {
+			Thread storageThread = new Thread() {			
+				public void run () {
+					dispatcher.queueStorageThread();
+				}
+			};	
+			storageThread.start();
+		}
+		
+		//Specify some jobs
+		dispatcher.specifyJob(3, 1);
+		dispatcher.specifyJob(2, 1);
+				
+		//Create more compute threads
+		for (int i = 0; i < 3; i++) {
+			Thread computeThread = new Thread() {			
+				public void run () {
+					dispatcher.queueComputeThread();
+				}
+			};	
+			computeThread.start();
+		}
+		
+		//Create more storage threads
+		for (int i = 0; i < 4; i++) {
+			Thread storageThread = new Thread() {			
+				public void run () {
+					dispatcher.queueStorageThread();
+				}
+			};	
+			storageThread.start();
+		}
+		
+		//Specify more jobs
+		dispatcher.specifyJob(0, 3);
+		dispatcher.specifyJob(6, 6); //This one is supposed to be left hanging
+		
+		//Wait for set time and assume that execution has finished:
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace(); };
+		
+		assertEquals(0, dispatcher.getWaitingCompute());
+		assertEquals(0, dispatcher.getWaitingStorage());
+		assertEquals(1, dispatcher.getJobListSize());
+	}
 	
 }
